233c0da8c0d750018ce957034d030bef6a9be962
Damian <paluzdam@gmail.com>
2023-08-09 00:45:27 -0300

    links

--- src/capitulos.jsx
+++ src/capitulos.jsx
@@ -1,6 +1,10 @@
 import imagen from './imagen.jsx'
 import editChapter from './editar.js';
 
+const olympusLink = "https://olympusv2.gg/capitulo/";
+const samuraiLink = "https://samuraiscan.com/leer/";
+const yugenmangasLink = "https://yugenmangas.net/series/";
+const knightnoscanLink = "https://knightnoscanlation.com/sr/";
 export const items = [
     {
         id: "0000",
@@ -12,21 +16,21 @@ export const items = [
     {
         id: "0001",
         name: "YUAN ZUN",
-        link: "https://samuraiscan.com/leer/yuan-zun/capitulo-",
+        link: samuraiLink + "yuan-zun/capitulo-",
         image: imagen.yuan,
         chapter: "892"
     },
     {
         id: "0002",
         name: "SECOND LIFE RANKER",
-        link: "https://yugenmangas.net/series/segunda-vida-para-ser-un-ranker/capitulo-",
+        link: yugenmangasLink + "segunda-vida-para-ser-un-ranker/capitulo-",
         image: imagen.ranker,
         chapter: "145"
     },
     {
         id: "0003",
         name: "REY ESPADA",
-        link: "https://knightnoscanlation.com/sr/el-rey-de-la-espada-en-un-mundo-de-fantasia/capitulo-",
+        link: knightnoscanLink + "el-rey-de-la-espada-en-un-mundo-de-fantasia/capitulo-",
         image: imagen.espada,
         chapter: "156"
     },
@@ -34,14 +38,14 @@ export const items = [
     {
         id: "0007",
         name: "CAZADOR SUIC1D4",
-        link: "https://yugenmangas.net/series/cazador-suicida-de-clase-sss/capitulo-",
+        link: yugenmangasLink + "cazador-suicida-de-clase-sss/capitulo-",
         image: imagen.suic1d3,
         chapter: "806"
     },
     {
         id: "0008",
         name: "RANKER OSO",
-        link: "https://yugenmangas.net/series/el-retorno-del-ranker-remake/capitulo-",
+        link: yugenmangasLink + "el-retorno-del-ranker-remake/capitulo-",
         image: imagen.osito,
         chapter: "84"
     },
@@ -49,21 +53,21 @@ export const items = [
     {
         id: "0012",
         name: "FRANCOTIRADOR",
-        link: "https://samuraiscan.com/leer/el-francotirador-de-nivel-arcano/capitulo-",
+        link: samuraiLink + "el-francotirador-de-nivel-arcano/capitulo-",
         image: imagen.arcane,
         chapter: "97"
     },
     {
         id: "0013",
         name: "COME LIBROS",
-        link: "https://yugenmangas.net/series/el-mago-comedor-de-libros/capitulo-",
+        link: yugenmangasLink + "el-mago-comedor-de-libros/capitulo-",
         image: imagen.libro,
         chapter: "80"
     },
     {
         id: "0014",
         name: "DEVORADOR",
-        link: "https://yugenmangas.net/series/archimago-devorador-de-talentos/capitulo-",
+        link: yugenmangasLink + "archimago-devorador-de-talentos/capitulo-",
         image: imagen.devorador,
         chapter: "50"
     },
@@ -73,7 +77,7 @@ export const olympus = [
     {
         id: "0004",
         name: "ESQUELETON SOLDIER",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "31185",
         link2: "/comic-soldado-esqueleto",
         image: imagen.esqueleto,
@@ -82,7 +86,7 @@ export const olympus = [
     {
         id: "0005",
         name: "RANKER SSS",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "35154",
         link2: "/comic-regreso-del-ranker-de-clase-sss",
         image: imagen.hacha,
@@ -91,7 +95,7 @@ export const olympus = [
     {
         id: "0006",
         name: "OVERGEARED",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "34776",
         link2: "/comic-overgear3d",
         image: imagen.overgeared,
@@ -100,7 +104,7 @@ export const olympus = [
     {
         id: "0009",
         name: "CABALLERO LANZA",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "34066",
         link2: "/comic-el-regreso-del-legendario-caballero-de-la-lanza",
         image: imagen.lanza,
@@ -109,7 +113,7 @@ export const olympus = [
     {
         id: "0010",
         name: "HARD CARRY SUPPORT",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "34874",
         link2: "/comic-hard-carry-support",
         image: imagen.soporte,
@@ -118,16 +122,16 @@ export const olympus = [
     {
         id: "0011",
         name: "NANOMAQUINAS",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "27302",
-        link2: "/comic-nano-maquinas",
+        link2: "/comic-el-maquina",
         image: imagen.nano,
         chapter: "142"
     },
     {
         id: "0100",
         name: "PLAYER",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "28472",
         link2: "/comic-nueva-vida-del-jugador",
         image: imagen.player,
@@ -137,7 +141,7 @@ export const olympus = [
         id: "0200",
         name: "NECROMANCER",
         link: "https://olympusscans.com/capitulo/65888/comic-subiendo-de-nivel-con-las-mejores-habilidades",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "64919",
         link2: "/comic-el-nigromante-de-seul",
         image: imagen.necromancer,
@@ -146,7 +150,7 @@ export const olympus = [
     {
         id: "0201",
         name: "MAGO INFINITO",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "67043",
         link2: "/comic-el-mago-infinito",
         image: imagen.infinito,
@@ -155,7 +159,7 @@ export const olympus = [
     {
         id: "1005",
         name: "MONTE HUA",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         link: "https://olympusscans.com/capitulo/65888/comic-subiendo-de-nivel-con-las-mejores-habilidades",
         midlink: "39782",
         link2: "/comic-el-regreso-de-la-secta-del-monte-hua",
@@ -165,8 +169,8 @@ export const olympus = [
     {
         id: "0016",
         name: "DESTINO F",
-        link: "https://olympusscanlation.com/manga/el-gran-sabio-de-la-humanidad/capitulo-",
-        link1: "https://olympusscans.com/capitulo/",
+        link: olympusLink,
+        link1: olympusLink,
         midlink: "60991",
         link2: "/comic-cazador-de-destinos-de-clase-f",
         image: imagen.destino,
@@ -176,7 +180,7 @@ export const olympus = [
         id: "0100",
         name: "MULTIPLICADOR",
         link: "https://olympusscans.com/capitulo/65888/comic-subiendo-de-nivel-con-las-mejores-habilidades",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "65888",
         link2: "/comic-subiendo-de-nivel-con-las-mejores-habilidades",
         image: imagen.multiplicador,
@@ -186,7 +190,7 @@ export const olympus = [
         id: "0101",
         name: "BARBARO",
         link: "https://olympusscans.com/capitulo/65888/comic-subiendo-de-nivel-con-las-mejores-habilidades",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "65888",
         link2: "/comic-sobreviviendo-al-juego-siendo-un-barbaro",
         image: imagen.barbaro,
@@ -196,7 +200,7 @@ export const olympus = [
         id: "0202",
         name: "DEMONIO",
         link: "https://olympusscans.com/capitulo/65888/comic-subiendo-de-nivel-con-las-mejores-habilidades",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "67460",
         link2: "/comic-evolucion-demoniaca",
         image: imagen.demon,
@@ -205,7 +209,7 @@ export const olympus = [
     {
         id: "0203",
         name: "CORRUPCION",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "20845",
         link2: "/comic-el-sacerdote-de-la-corrupcion",
         image: imagen.priest,
@@ -214,7 +218,7 @@ export const olympus = [
     {
         id: "2214",
         name: "MAGO DORADO",
-        link1: "https://olympusscans.com/capitulo/",
+        link1: olympusLink,
         midlink: "20845",
         link2: "/comic-mago-dorado",
         image: imagen.gold,
@@ -232,7 +236,7 @@ export const olympus = [
 
 ]
 
-editChapter(items, "MARTIAL PEAK", "3500");
+editChapter(items, "MARTIAL PEAK", "3504");
 
 editChapter(items, "YUAN ZUN", "983");
 
@@ -240,7 +244,7 @@ editChapter(items, "second life ranker", "161");
 
 editChapter(items, "REY ESPADA", "179");
 
-editChapter(items, "CAZADOR SUIC1D4", "90");
+editChapter(items, "CAZADOR SUIC1D4", "91");
 
 editChapter(items, "RANKER OSO", "108");
 
@@ -253,9 +257,9 @@ editChapter(items, "DEVORADOR", "50");
 //-------------------------------------------------------
 editChapter(olympus, "ESQUELETON SOLDIER", "247", "70253");
 
-editChapter(olympus, "RANKER SSS", "70", "71830");
+editChapter(olympus, "RANKER SSS", "71", "72535");
 
-editChapter(olympus, "OVERGEARED", "189", "70038");
+editChapter(olympus, "OVERGEARED", "190", "70888");
 
 editChapter(olympus, "CABALLERO LANZA", "99", "66507");
 
@@ -273,9 +277,9 @@ editChapter(olympus, "PLAYER", "52", "70273");
 
 editChapter(olympus, "MONTE HUA", "21", "39782");
 
-editChapter(olympus, "DESTINO F", "33", "71213");
+editChapter(olympus, "DESTINO F", "34", "72113");
 
-editChapter(olympus, "MULTIPLICADOR", "28   ", "71304");
+editChapter(olympus, "MULTIPLICADOR", "29", "72168");
 
 editChapter(olympus, "BARBARO", "24", "72079");
 
